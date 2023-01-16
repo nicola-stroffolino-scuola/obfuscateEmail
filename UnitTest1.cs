37f53a029b47b7ac402e8da93f5272f1a0219deb
ricbianchi <info@riccardobianchi.net>
2023-01-16 09:50:27 +0100

    Aggiunti alcuni test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,37 +4,47 @@ public class UnitTest1
 {
     [Fact]
     public void Test1()
-    {   
+    {
         Assert.Equal("i***@c************i.net", ObfuscationServices.ObfuscateEmail("info@carmelocarmeli.net"));
     }
     [Fact]
     public void Test2()
-    {   
+    {
         Assert.Equal("i***2@c*************i.net", ObfuscationServices.ObfuscateEmail("info2@carmelo.carmeli.net"));
     }
     [Fact]
     public void Test3()
-    {   
+    {
         Assert.Equal("i**********e@c*************i.net", ObfuscationServices.ObfuscateEmail("info.vendite@carmelo.carmeli.net"));
     }
     [Fact]
     public void Test4()
-    {   
+    {
         Assert.Equal("Indirizzo mail non valido", ObfuscationServices.ObfuscateEmail("infocarmelocarmeli.net"));
     }
     [Fact]
     public void Test5()
-    {   
+    {
         Assert.Equal("Indirizzo mail non valido", ObfuscationServices.ObfuscateEmail("info@carmelocarmelinet"));
     }
     [Fact]
     public void Test6()
-    {   
+    {
         Assert.Equal("Indirizzo mail non valido", ObfuscationServices.ObfuscateEmail("info@carmelo@carmeli.net"));
     }
     [Fact]
     public void Test7()
-    {   
-        Assert.Equal("i***@c************i.info", ObfuscationServices.ObfuscateEmail("info@carmelocarmeli.info"));
+    {
+        Assert.Equal("Indirizzo mail non valido", ObfuscationServices.ObfuscateEmail("@carmelocarmeli.net"));
+    }
+    [Fact]
+    public void Test8()
+    {
+        Assert.Equal("Indirizzo mail non valido", ObfuscationServices.ObfuscateEmail("info@carmelocarmeli.n"));
+    }
+    [Fact]
+    public void Test9()
+    {
+        Assert.Equal("i***@c************i.it", ObfuscationServices.ObfuscateEmail("info@carmelocarmeli.it"));
     }
 }
