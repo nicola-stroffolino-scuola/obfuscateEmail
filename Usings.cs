b08e3c659ccbc1ca3869d31a295beab5267dd0fd
HotBaconWasTaken <nicola.stroffolino@studenti.ittsrimini.edu.it>
2023-01-18 20:46:07 +0100

    Corrected Variable Names

--- Usings.cs
+++ Usings.cs
@@ -8,15 +8,15 @@ public static class ObfuscationServices
         // controllo del corretto uso delle @
         if(emailAddress[0] == '@' || emailAddress.Split('@').Length != 2) return "Indirizzo mail non valido"; 
 
-        string local = emailAddress.Split('@')[0];
+        string userName = emailAddress.Split('@')[0];
         string domain = emailAddress.Split('@')[1];
         // controllo dei numeri di punti dopo la @
         if(domain.Split('.').Length < 2) return "Indirizzo mail non valido"; 
 
         // example --> e*****e
-        local = string.Join("", local.ToCharArray().Select(x => '*'));
-        if(local.Length > 4) local = emailAddress[0] + local.Substring(1, local.Length - 2) + emailAddress[local.Length - 1];
-        else local = emailAddress[0] + local.Substring(1);
+        userName = string.Join("", userName.ToCharArray().Select(x => '*'));
+        if(userName.Length > 4) userName = emailAddress[0] + userName.Substring(1, userName.Length - 2) + emailAddress[userName.Length - 1];
+        else userName = emailAddress[0] + userName.Substring(1);
 
         // gmail.com --> g***l.com
         string tld = domain.Substring(domain.LastIndexOf('.') + 1);
@@ -24,8 +24,8 @@ public static class ObfuscationServices
         if(tld.Length < 2) return "Indirizzo mail non valido";
         domain = domain.Remove(domain.Length - tld.Length - 1);
         domain = string.Join("", domain.ToCharArray().Select(x => '*'));
-        domain = emailAddress[local.Length + 1] + domain.Substring(1, domain.Length - 2) + emailAddress[local.Length + domain.Length];
+        domain = emailAddress[userName.Length + 1] + domain.Substring(1, domain.Length - 2) + emailAddress[userName.Length + domain.Length];
         
-        return local + '@' + domain + '.' + tld;
+        return userName + '@' + domain + '.' + tld;
     }
 }
